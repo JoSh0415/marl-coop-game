84a42f699ee32a9505aa63b301936a98ef7aabcc
Josh Deane <66123746+JoSh0415@users.noreply.github.com>
2025-12-11 14:49:12 +0000

    add unit tests for the environment

---
+++ environment/__init__.py
@@ -0,0 +1,3 @@
+from .env import CoopEnv, find_char, COOK_TIME, BURN_TIME
+
+__all__ = ["CoopEnv", "find_char", "COOK_TIME", "BURN_TIME"]
--- environment/marl_coop_env.py
+++ environment/marl_coop_env.py
@@ -34,7 +34,7 @@ display.set_caption("MARL Cooperative Environment")
 
 clock = time.Clock()
 
-env = CoopEnv(LEVEL1, ORDERS1, tile_size=tile_size, max_steps=6600, order_time = 3600, header_size=header_size)
+env = CoopEnv(LEVEL1, ORDERS1, tile_size=tile_size, max_steps=6600, order_time = 4200, header_size=header_size)
 obs = env.reset()
 
 running = True
