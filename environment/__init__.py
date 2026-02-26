0d3b7b0ca3910af6fe24d016dc356c12ed20dac2
Josh Deane <joshuabournville@gmail.com>
2026-02-26 00:16:02 +0000

    Add centralised PPO training script and GymCoopEnvRLlib environment

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,4 +1,5 @@
 from .env import CoopEnv, find_char, COOK_TIME, BURN_TIME
 from .gym_wrapper import GymCoopEnv
+from .gym_wrapper_rllib import GymCoopEnvRLlib
 
-__all__ = ["CoopEnv", "find_char", "COOK_TIME", "BURN_TIME", "GymCoopEnv"]
+__all__ = ["CoopEnv", "find_char", "COOK_TIME", "BURN_TIME", "GymCoopEnv", "GymCoopEnvRLlib"]
